7259dd9be701746234dc9df22a89ab0ac03be863
watanabbee <144165798+watanabbee@users.noreply.github.com>
2024-12-02 08:34:19 -0300

    Spawn de player e fantasma

--- pacman.py
+++ pacman.py
@@ -1,1 +1,40 @@
-print('olá augusto')
+import numpy as np
+import os
+import random
+
+mapa = np.array([
+    ["#","#","#","#","#","#","#","#","#"],
+    ["#"," "," "," "," "," "," "," ","#"],
+    ["#"," "," "," "," "," "," "," ","#"],
+    ["#"," "," "," "," "," "," "," ","#"],
+    ["#"," "," "," "," "," "," "," ","#"],
+    ["#"," "," "," "," "," "," "," ","#"],
+    ["#","#","#","#","#","#","#","#","#"]
+])
+
+def exibir_mapa():
+    os.system('cls' if os.name == 'nt' else 'clear')  # Limpa a tela para atualização do mapa
+    for linha in mapa:
+        print("".join(linha))  # Junta os caracteres da linha em uma string
+
+def spawn_player():
+    pos_x = random.randint(1,6)
+    pos_y = random.randint(1,6)
+    if mapa[pos_x,pos_y]==' ':
+        mapa[pos_x,pos_y] ='c'
+        exibir_mapa()
+    else:
+        spawn_player()
+
+def spawn_fantasma():
+    pos_x = random.randint(1,6)
+    pos_y = random.randint(1,6)
+    if mapa[pos_x,pos_y]==' ':
+        mapa[pos_x,pos_y] ='f'
+        exibir_mapa()
+    else:
+        spawn_fantasma()
+
+spawn_player()
+spawn_fantasma()
+exibir_mapa()
